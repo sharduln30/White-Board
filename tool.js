54c3f6a6daf24e90f96d5e4ae67e742ac7f0b2da
Shardul Negi <shardulnegi12@gmail.com>
2020-08-01 13:12:15 +0530

    Added static Sticky Notes

--- tool.js
+++ tool.js
@@ -1,4 +1,6 @@
 let activeTool = "pencil";
+let pencilOptions = document.getElementById("p-options");
+let eraserOptions = document.getElementById("e-options");
 let tools = document.querySelectorAll(".tool-img");
 
 for (let i = 0; i < tools.length; i++) {
@@ -8,10 +10,38 @@ for (let i = 0; i < tools.length; i++) {
         let elem = e.currentTarget;
         let toolName = elem.getAttribute("id");
         if (toolName == "eraser") {
-            // color white
-            ctx.strokeStyle = "white";
-        } else {
-            ctx.strokeStyle = "black";
+            // color whiteif
+            if (activeTool == "eraser") {
+                eraserOptions.classList.add("show");
+            } else {
+                activeTool = "eraser"
+                pencilOptions.classList.remove("show")
+                ctx.strokeStyle = "white";
+            }
+        } else if (toolName == "pencil") {
+            if (activeTool == "pencil") {
+                pencilOptions.classList.add("show");
+
+            } else {
+                activeTool == "pencil";
+                eraserOptions.classList.remove("show");
+                ctx.strokeStyle = "black";
+            }
         }
+    })
+}
+
+function handleColorChange(color) {
+    ctx.strokeStyle = color;
+}
+
+let allSliders = document.querySelectorAll(".slider");
+for (let i = 0; i < allSliders.length; i++) {
+    allSliders[i].addEventListener("change", function (e) {
+        console.log(e);
+        // element on which event occured
+        let elem = e.currentTarget;
+        let newSize = elem.value;
+        ctx.lineWidth = newSize;
     })
 }
